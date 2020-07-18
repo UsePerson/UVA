f87fead0e71d145aee6bfc4ba74bb273fe32287a
UsePerson <58616866+UsePerson@users.noreply.github.com>
2020-07-18 11:38:12 +0800

    Updata uva10608 java

--- V100/uva10608.java
+++ V100/uva10608.java
@@ -1,16 +1,14 @@
-package aaa;
-// UVA 10608 Friends
 import java.util.*;
 public class Union 
 {
-	public static int set[] = new int [30001];	// town¡¦s citizens ( 1 <= N <= 30000 )
+	public static int set[] = new int [30001];	// townÂ¡Â¦s citizens ( 1 <= N <= 30000 )
 	public static void main( String[] args ){ 
 	
 		Scanner input = new Scanner( System.in );
 		int test = input.nextInt();	// get test case
 		for( int i = 1 ; i <= test ; i ++ ){
 		
-			int N = input.nextInt();	// get town¡¦s citizens ( 1 <= N <= 30000 )
+			int N = input.nextInt();	// get townÂ¡Â¦s citizens ( 1 <= N <= 30000 )
 			int M = input.nextInt();	// get pairs of people ( 0 <= M <= 500000)
 			init( N );
 			for( int j = 1 ; j <= M ; j ++ ){
